6215dfe62e3cc0c25f77276f7d8e29864471978d
babbage <601728820@qq.com>
2021-02-19 17:28:25 +0800

    fix: merge.sh 没有去重问题修复,合并文件名微调，已实现各个阶段文件都带一个统一id

--- corpus/merge.sh
+++ corpus/merge.sh
@@ -11,9 +11,12 @@ echo "处理之前语料总量统计信息:"
 ls ./sources/**/*.txt | xargs -n 1 -I {} wc -l {} | awk -F " " '{sum += $1} END {print sum}'
 
 merge_id=$(date +%Y%m%d%H%M%S)
-new_file="merged_data_sets/merged_corpus_${merge_id}".txt
+new_file="merged_data_sets/v1_${merge_id}".txt
 
-cat ./sources/**/*.txt >>$new_file
+cat ./sources/**/*.txt >>$merge_id
+# 去重
+sort -u $merge_id >$new_file
+rm $merge_id
 
 echo "已合并，统计信息如下:"
 wc -l $new_file
